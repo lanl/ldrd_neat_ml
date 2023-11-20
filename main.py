c94c10afe8819846967a9625a87ae4f7294f3672
Tyler Reddy <treddy@lanl.gov>
2023-11-20 16:43:32 -0700

    ENH: add hyperparameter search
    * add a skeleton for hyperparameter searching; at the moment
    it improves some models and makes others worse, but there
    is so little data at the moment that the focus is just to lay
    the groundwork for the incoming postdoc
    
    * sample output from running on this branch: `python main.py`
    
    ```
    ----------------------------------------------------------------------
    baseline estimator average CV ROC AUC score data on training only:
    estimator_name: rfc, average AUC: 0.8333333333333334
    estimator_name: xgb_class, average AUC: 0.8666666666666668
    estimator_name: xgb_dart, average AUC: 0.8
    estimator_name: svm, average AUC: 0.8
    ----------------------------------------------------------------------
    ----------------------------------------------------------------------
    Hyperparameter optimization on training only:
    setting best params for rfc estimator: {'max_depth': 1, 'min_samples_split': 2}
    setting best params for xgb_class estimator: {'colsample_bytree': 1, 'n_estimators': 100, 'subsample': 0.9}
    setting best params for xgb_dart estimator: {'colsample_bytree': 1, 'n_estimators': 300, 'subsample': 1}
    setting best params for svm estimator: {'C': 1, 'kernel': 'linear'}
    estimator_name: rfc, average AUC: 0.8666666666666668
    estimator_name: xgb_class, average AUC: 0.8333333333333334
    estimator_name: xgb_dart, average AUC: 0.8666666666666668
    estimator_name: svm, average AUC: 0.8666666666666668
    ----------------------------------------------------------------------
    ```

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import (train_test_split, cross_val_score,
-                                     StratifiedKFold)
+                                     StratifiedKFold, GridSearchCV)
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
@@ -19,7 +19,6 @@ def main():
     # we only have two features at the moment (% Dextran, % PEO)
     # so no need for feature selection just yet; can jump right into
     # hyperparameter optimization
-    # TODO: add hyperparameter optimization
 
     # Step 2: Split the data into training and test sets
     # we don't have much data for now, but we still need
@@ -41,11 +40,17 @@ def main():
                       "xgb_dart": {"classifier": xgb.XGBClassifier(booster="dart",
                                                                    one_drop=1)},
                       # sklearn SVM:
-                      "svm": {"classifier": make_pipeline(StandardScaler(), SVC(gamma='auto'))},
+                      "svm": {"classifier": SVC(gamma='auto')},
                       }
 
     for estimator_name in estimator_data.keys():
-        scores = cross_val_score(estimator_data[estimator_name]["classifier"],
+        estimator = estimator_data[estimator_name]["classifier"]
+        if estimator_name == "svm":
+            # need the scaler for SVM
+            estimator = make_pipeline(StandardScaler(), estimator)
+            # TODO: safety check per:
+            # https://stackoverflow.com/a/43366811/2942522
+        scores = cross_val_score(estimator,
                                  X_train,
                                  y_train,
                                  scoring="roc_auc",
@@ -62,7 +67,39 @@ def main():
     print("-" * 70)
 
     # Step 4: Perform some basic hyperparameter searching/optimization
-    # TODO
+    # NOTE: at the moment, we have so little data that this optimization
+    # is really just here as a skeleton for a larger data/workflow in the future
+    # When we need to handle a larger search space, we may need to use i.e.,
+    # RandomizedSearchCV as a compromise over exhaustive grid searching.
+    print("-" * 70)
+    print("Hyperparameter optimization on training only:")
+    for estimator_name in estimator_data.keys():
+        hyperparam_input = lib.hyper_param_dict[estimator_name]
+        cls = estimator_data[estimator_name]["classifier"]
+        # TODO: scaler for SVM/pipeline handling
+        # careful: https://stackoverflow.com/a/43366811/2942522
+        clf = GridSearchCV(cls,
+                           hyperparam_input)
+        clf.fit(X_train, y_train)
+        print(f"setting best params for {estimator_name} estimator:", clf.best_params_)
+        estimator = estimator_data[estimator_name]["classifier"]
+        estimator.set_params(**clf.best_params_)
+        if estimator_name == "svm":
+            # need the scaler for SVM
+            estimator = make_pipeline(StandardScaler(), estimator)
+        scores = cross_val_score(estimator,
+                                 X_train,
+                                 y_train,
+                                 scoring="roc_auc",
+                                 cv=StratifiedKFold(5))
+        new_score = np.average(scores)
+        estimator_data[estimator_name]["hyperparam_average_auc"] = new_score
+    for estimator_name in estimator_data.keys():
+        print(f"estimator_name: {estimator_name},",
+              "average AUC:",
+              estimator_data[estimator_name]["hyperparam_average_auc"]
+              )
+    print("-" * 70)
 
 
 if __name__ == "__main__":
