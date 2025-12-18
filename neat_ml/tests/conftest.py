6b74a841248e51d7a59fb1717fd360dbfd7e99aa
Adam <awitmer@lanl.gov>
2025-12-18 10:55:50 -0700

    MAINT: add manual download of test files with requests

--- neat_ml/tests/conftest.py
+++ neat_ml/tests/conftest.py
@@ -5,6 +5,8 @@ from importlib import resources
 from typing import Generator, Any
 import pooch  # type: ignore[import-untyped]
 from matplotlib import rcParams
+import requests
+import os
 
 # try setting plot font to ``Arial``, if installed, 
 # otherwise default to standard matplotlib font
@@ -38,13 +40,7 @@ def baseline_dir() -> Generator[Any, Any, Any]:
 image_files = pooch.create(
     base_url = "doi:10.5281/zenodo.17545141",
     path = pooch.os_cache("test_images")
-)
-# backup storage of image files in case of
-# error due to too many parallel requests to zenodo
-image_files_backup = pooch.create(
-    base_url = "doi:10.6084/m9.figshare.30546491", 
-    path = pooch.os_cache("test_images")
-)
+) 
 
 @pytest.fixture(scope="session")
 def reference_images():
@@ -63,19 +59,26 @@ def reference_images():
              fname="images_raw.tiff", 
         )
     except ValueError:
-        image_files_backup.load_registry_from_doi()
-        images_Processed_raw = image_files_backup.fetch(
-             fname="images_Processed_raw.tiff",
-        )
-        detection_raw = image_files_backup.fetch(
-         fname="raw_detection.png",
-        ) 
-        detection_processed = image_files_backup.fetch(
-             fname = "raw_processed.png",
-        )
-        images_raw = image_files_backup.fetch(
-             fname="images_raw.tiff", 
-        )
+        output_paths = []
+        file_save_path = pooch.os_cache("test_images")
+        # try manual download from figshare backup
+        image_files_backup = requests.get("https://api.figshare.com/v2/articles/30546491")
+        image_files_info = image_files_backup.json()
+        image_files_data = image_files_info.get("files")
+        for image_file in image_files_data:
+            file_name = image_file["name"]
+            download_url = image_file["download_url"]
+            image_download = requests.get(download_url)
+            output_path = os.path.join(file_save_path, file_name)
+            output_paths.append(output_path)
+            if not os.path.exists(output_path):
+                with open(output_path, "wb") as f:
+                    for out_file in image_download.iter_content():
+                        f.write(out_file)  
+        # reorder paths based on pooch order
+        output_imgs = tuple(output_paths[i] for i in [0, 3, 2, 1])        
+        return output_imgs
+
     return (images_Processed_raw,
             detection_raw,
             detection_processed,
