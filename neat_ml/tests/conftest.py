bc3b2ff7e2e9e068a9dc222e6f4fd78cb21e830d
Adam Witmer <adamwitmer@gmail.com>
2025-12-30 12:53:35 -0800

    MAINT: add repo for storing/downloading pooch images
    * add repo for storing images at https://github.com/adamwitmer/ldrd_neat_ml_images
    * fix pooch fetcher to use new repo/image registry

--- neat_ml/tests/conftest.py
+++ neat_ml/tests/conftest.py
@@ -5,8 +5,6 @@ from importlib import resources
 from typing import Generator, Any
 import pooch  # type: ignore[import-untyped]
 from matplotlib import rcParams
-import requests
-import os
 
 # try setting plot font to ``Arial``, if installed, 
 # otherwise default to standard matplotlib font
@@ -38,47 +36,31 @@ def baseline_dir() -> Generator[Any, Any, Any]:
 # image files stored at the following url:
 # https://zenodo.org/records/17545141
 image_files = pooch.create(
-    base_url = "doi:10.5281/zenodo.17545141",
-    path = pooch.os_cache("test_images")
+    base_url = "https://github.com/adamwitmer/ldrd_neat_ml_images/raw/main/test_images",
+    path = pooch.os_cache("test_images"),
+    registry = {
+        "images_Processed_raw.tiff": "sha256:cc145dde89791119f59c3aaa75d12e1f23c2da6d87f02eabde50b2ab7ea648cb",
+        "images_raw.tiff": "sha256:9ac34743ceb6449f93888e668cb6c1f79470434efd46aeedb6b9185173e6725b",
+        "raw_detection.png": "sha256:496af9670a23c998cb50a6e00c46b46f915ea4110b4b62a382f901f5ad469d19",
+        "raw_processed.png": "sha256:c5d9cdd527f87eb2d198d2d28717c40c5064ef0848707a7eb4f5f9e8a9f5b3c6",        
+    }
 ) 
 
 @pytest.fixture(scope="session")
 def reference_images():
-    try:
-        image_files.load_registry_from_doi()
-        images_Processed_raw = image_files.fetch(
-             fname="images_Processed_raw.tiff",
-        )
-        detection_raw = image_files.fetch(
-         fname="raw_detection.png",
-        ) 
-        detection_processed = image_files.fetch(
-             fname = "raw_processed.png",
-        )
-        images_raw = image_files.fetch(
-             fname="images_raw.tiff", 
-        )
-    except ValueError:
-        output_paths = []
-        file_save_path = pooch.os_cache("test_images")
-        # try manual download from figshare backup
-        image_files_backup = requests.get("https://api.figshare.com/v2/articles/30546491")
-        image_files_info = image_files_backup.json()
-        image_files_data = image_files_info.get("files")
-        for image_file in image_files_data:
-            file_name = image_file["name"]
-            download_url = image_file["download_url"]
-            image_download = requests.get(download_url)
-            output_path = os.path.join(file_save_path, file_name)
-            output_paths.append(output_path)
-            if not os.path.exists(output_path):
-                with open(output_path, "wb") as f:
-                    for out_file in image_download.iter_content():
-                        f.write(out_file)  
-        # reorder paths based on pooch order
-        output_imgs = tuple(output_paths[i] for i in [0, 3, 2, 1])        
-        return output_imgs
-
+    images_Processed_raw = image_files.fetch(
+         fname="images_Processed_raw.tiff",
+    )
+    detection_raw = image_files.fetch(
+     fname="raw_detection.png",
+    ) 
+    detection_processed = image_files.fetch(
+         fname = "raw_processed.png",
+    )
+    images_raw = image_files.fetch(
+         fname="images_raw.tiff", 
+    )
+    
     return (images_Processed_raw,
             detection_raw,
             detection_processed,
